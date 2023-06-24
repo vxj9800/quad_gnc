7299af5eed197e789fc1c465c6ed262610ce03f9
Vatsal Joshi <vatsalasitkumar.joshi@mavs.uta.edu>
2023-06-24 14:50:25 -0500

    Update variable names in kbInput

--- include/kbInput.hpp
+++ include/kbInput.hpp
@@ -15,6 +15,7 @@
  * @brief This class is a parallel of RC input where the input is taken from a keyboard and the values are published as RC signal.
  * 
 */
+
 class kbInput : public rclcpp::Node
 {
 public:
@@ -22,10 +23,10 @@ public:
 
 private:
     void kbInputPublish(void);
-    rclcpp::Publisher<std_msgs::msg::UInt16>::SharedPtr roll_pub, pitch_pub, yaw_pub, thrust_pub;
-    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr arm_pub;
-    std_msgs::msg::UInt16 roll, pitch, yaw, thrust;
-    std_msgs::msg::Bool arm;
+    rclcpp::Publisher<std_msgs::msg::UInt16>::SharedPtr rcRollPub, rcPitchPub, rcYawPub, rcThrustPub;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr rcArmPub;
+    std_msgs::msg::UInt16 rcRoll, rcPitch, rcYaw, rcThrust;
+    std_msgs::msg::Bool rcArm;
     rclcpp::TimerBase::SharedPtr timer;
 };
 
--- src/kbInput.cpp
+++ src/kbInput.cpp
@@ -3,25 +3,25 @@
 kbInput::kbInput() : Node("kbInput")
 {
 	// Create the publishers
-	roll_pub = this->create_publisher<std_msgs::msg::UInt16>("rc/roll", 10);
-	pitch_pub = this->create_publisher<std_msgs::msg::UInt16>("rc/pitch", 10);
-	yaw_pub = this->create_publisher<std_msgs::msg::UInt16>("rc/yaw", 10);
-	thrust_pub = this->create_publisher<std_msgs::msg::UInt16>("rc/thrust", 10);
-	arm_pub = this->create_publisher<std_msgs::msg::Bool>("rc/arm", 10);
+	rcRollPub = this->create_publisher<std_msgs::msg::UInt16>("rc/roll", 10);
+	rcPitchPub = this->create_publisher<std_msgs::msg::UInt16>("rc/pitch", 10);
+	rcYawPub = this->create_publisher<std_msgs::msg::UInt16>("rc/yaw", 10);
+	rcThrustPub = this->create_publisher<std_msgs::msg::UInt16>("rc/thrust", 10);
+	rcArmPub = this->create_publisher<std_msgs::msg::Bool>("rc/arm", 10);
 
 	// Set default message values
-	roll.data = 1500;
-	pitch.data = 1500;
-	yaw.data = 1500;
-	thrust.data = 1000;
-	arm.data = 0;
+	rcRoll.data = 1500;
+	rcPitch.data = 1500;
+	rcYaw.data = 1500;
+	rcThrust.data = 1000;
+	rcArm.data = 0;
 
 	// Publish default values
-	roll_pub->publish(roll);
-	pitch_pub->publish(pitch);
-	yaw_pub->publish(yaw);
-	thrust_pub->publish(thrust);
-	arm_pub->publish(arm);
+	rcRollPub->publish(rcRoll);
+	rcPitchPub->publish(rcPitch);
+	rcYawPub->publish(rcYaw);
+	rcThrustPub->publish(rcThrust);
+	rcArmPub->publish(rcArm);
 
 	// Create a one-shot timer to call kbInputPublisher function
 	timer = this->create_wall_timer(std::chrono::milliseconds(50), std::bind(&kbInput::kbInputPublish, this));
@@ -34,42 +34,42 @@ void kbInput::kbInputPublish()
 
 	// Get the spring loaded sticks back to default position if it was not pressed last time
 	if ((cLast != 'a') && (cLast != 'd'))
-		yaw.data = 1500;
+		rcYaw.data = 1500;
 	if ((cLast != 'j') && (cLast != 'l'))
-		roll.data = 1500;
+		rcRoll.data = 1500;
 	if ((cLast != 'i') && (cLast != 'k'))
-		pitch.data = 1500;
+		rcPitch.data = 1500;
 
 	// Look for specific key input and increase the input two times
 	switch ((c = getch()))
 	{
 	case 'w':
-		thrust.data += (thrust.data < 2000) ? 10 : 0;
+		rcThrust.data += (rcThrust.data < 2000) ? 10 : 0;
 		break;
 	case 'a':
-		yaw.data += (yaw.data < 2000) ? 10 : 0;
+		rcYaw.data += (rcYaw.data < 2000) ? 10 : 0;
 		break;
 	case 's':
-		thrust.data -= (thrust.data > 1000) ? 10 : 0;
+		rcThrust.data -= (rcThrust.data > 1000) ? 10 : 0;
 		break;
 	case 'd':
-		yaw.data -= (yaw.data > 1000) ? 10 : 0;
+		rcYaw.data -= (rcYaw.data > 1000) ? 10 : 0;
 		break;
 	case 'i':
-		pitch.data += (pitch.data < 2000) ? 10 : 0;
+		rcPitch.data += (rcPitch.data < 2000) ? 10 : 0;
 		break;
 	case 'j':
-		roll.data += (roll.data < 2000) ? 10 : 0;
+		rcRoll.data += (rcRoll.data < 2000) ? 10 : 0;
 		break;
 	case 'k':
-		pitch.data -= (pitch.data > 1000) ? 10 : 0;
+		rcPitch.data -= (rcPitch.data > 1000) ? 10 : 0;
 		break;
 	case 'l':
-		roll.data -= (roll.data > 1000) ? 10 : 0;
+		rcRoll.data -= (rcRoll.data > 1000) ? 10 : 0;
 		break;
 	case 'r':
-		if (roll.data == 1500 && pitch.data == 1500 && yaw.data == 1500 && thrust.data == 1000)
-			arm.data = !arm.data;
+		if (rcRoll.data == 1500 && rcPitch.data == 1500 && rcYaw.data == 1500 && rcThrust.data == 1000)
+			rcArm.data = !rcArm.data;
 		break;
 	default:
 		break;
@@ -78,11 +78,11 @@ void kbInput::kbInputPublish()
 	cLast = c;
 
 	// Publish the rc signal
-	thrust_pub->publish(thrust);
-	roll_pub->publish(roll);
-	pitch_pub->publish(pitch);
-	yaw_pub->publish(yaw);
-	arm_pub->publish(arm);
+	rcThrustPub->publish(rcThrust);
+	rcRollPub->publish(rcRoll);
+	rcPitchPub->publish(rcPitch);
+	rcYawPub->publish(rcYaw);
+	rcArmPub->publish(rcArm);
 
 	// Clean-up the input for unwanted key presses
 	flushinp();
