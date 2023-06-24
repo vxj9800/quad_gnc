6944b04fd459d33d0314525a12348f71e0087777
Vatsal Joshi <vatsalasitkumar.joshi@mavs.uta.edu>
2023-06-24 14:22:28 -0500

    Make kbInput behavior realistic

--- src/kbInput.cpp
+++ src/kbInput.cpp
@@ -24,77 +24,68 @@ kbInput::kbInput() : Node("kbInput")
 	arm_pub->publish(arm);
 
 	// Create a one-shot timer to call kbInputPublisher function
-	timer = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&kbInput::kbInputPublish, this));
+	timer = this->create_wall_timer(std::chrono::milliseconds(50), std::bind(&kbInput::kbInputPublish, this));
 }
 
 void kbInput::kbInputPublish()
 {
+	// Variables to store keypress values
+	static int c, cLast;
 
-	// Break condition variable
-	static bool ctrlC = 0;
-
-	// Reduce the values by one
-	thrust.data -= (thrust.data > 1000) ? 10 : 0;
-	yaw.data -= (yaw.data > 1500) ? 10 : 0;
-	yaw.data += (yaw.data < 1500) ? 10 : 0;
-	roll.data -= (roll.data > 1500) ? 10 : 0;
-	roll.data += (roll.data < 1500) ? 10 : 0;
-	pitch.data -= (pitch.data > 1500) ? 10 : 0;
-	pitch.data += (pitch.data < 1500) ? 10 : 0;
+	// Get the spring loaded sticks back to default position if it was not pressed last time
+	if ((cLast != 'a') && (cLast != 'd'))
+		yaw.data = 1500;
+	if ((cLast != 'j') && (cLast != 'l'))
+		roll.data = 1500;
+	if ((cLast != 'i') && (cLast != 'k'))
+		pitch.data = 1500;
 
 	// Look for specific key input and increase the input two times
-	if (!ctrlC)
+	switch ((c = getch()))
 	{
-		switch (getch())
-		{
-		case 'w':
-			thrust.data += (thrust.data < 2000) ? 20 : 0;
-			break;
-		case 'a':
-			yaw.data += (yaw.data < 2000) ? 20 : 0;
-			break;
-		case 's':
-			thrust.data -= (thrust.data > 1000) ? 20 : 0;
-			break;
-		case 'd':
-			yaw.data -= (yaw.data > 1000) ? 20 : 0;
-			break;
-		case 'i':
-			pitch.data += (pitch.data < 2000) ? 20 : 0;
-			break;
-		case 'j':
-			roll.data -= (roll.data < 2000) ? 20 : 0;
-			break;
-		case 'k':
-			pitch.data -= (pitch.data > 1000) ? 20 : 0;
-			break;
-		case 'l':
-			roll.data += (roll.data > 1000) ? 20 : 0;
-			break;
-		case 'r':
-			if (roll.data == 1500 && pitch.data == 1500 && yaw.data == 1500 && thrust.data == 1000)
-			{
-				arm.data = !arm.data;
-			}
-			else
-			{
-				printw("Can't arm. roll: %d, pitch: %d, yaw: %d, thrust: %d\r\n", roll.data, pitch.data, yaw.data, thrust.data);
-				refresh();
-			}
-			break;
-		case 'q': // Ctrl+C
-			ctrlC = true;
-			break;
-		default:
-			break;
-		}
-		thrust_pub->publish(thrust);
-		roll_pub->publish(roll);
-		pitch_pub->publish(pitch);
-		yaw_pub->publish(yaw);
-		arm_pub->publish(arm);
-		flushinp();
+	case 'w':
+		thrust.data += (thrust.data < 2000) ? 10 : 0;
+		break;
+	case 'a':
+		yaw.data += (yaw.data < 2000) ? 10 : 0;
+		break;
+	case 's':
+		thrust.data -= (thrust.data > 1000) ? 10 : 0;
+		break;
+	case 'd':
+		yaw.data -= (yaw.data > 1000) ? 10 : 0;
+		break;
+	case 'i':
+		pitch.data += (pitch.data < 2000) ? 10 : 0;
+		break;
+	case 'j':
+		roll.data += (roll.data < 2000) ? 10 : 0;
+		break;
+	case 'k':
+		pitch.data -= (pitch.data > 1000) ? 10 : 0;
+		break;
+	case 'l':
+		roll.data -= (roll.data > 1000) ? 10 : 0;
+		break;
+	case 'r':
+		if (roll.data == 1500 && pitch.data == 1500 && yaw.data == 1500 && thrust.data == 1000)
+			arm.data = !arm.data;
+		break;
+	default:
+		break;
 	}
+	// Record last keypress
+	cLast = c;
+
+	// Publish the rc signal
+	thrust_pub->publish(thrust);
+	roll_pub->publish(roll);
+	pitch_pub->publish(pitch);
+	yaw_pub->publish(yaw);
+	arm_pub->publish(arm);
+
+	// Clean-up the input for unwanted key presses
+	flushinp();
 }
 
 int main(int argc, char *argv[])
